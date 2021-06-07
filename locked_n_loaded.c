d78b48d0b2dd6545a209f2b8094b7a37e13bad40
Zunnoon Tahir <78951403+Zunnoont@users.noreply.github.com>
2021-06-07 20:53:52 +1000

    Adding movement and in-game lore

--- locked_n_loaded.c
+++ locked_n_loaded.c
@@ -4,25 +4,27 @@
 #include <time.h>
 
 
+
 #define SIZE 13
 #define EMPTY 0
 #define ENEMY 1
+
 int random_number(int upper, int lower){
     srand(time(0));
     int random_value =  (rand() % (upper - lower + 1)) + lower;
     return random_value;
 }
-void print_map (int map[SIZE][SIZE]) {
-    int spawn_x = random_number(12,0);
-    int spawn_y = random_number(12,0);
-   
+void print_map (int map[SIZE][SIZE], int spawn_x, int spawn_y) {
+    // generate random position on (x,y)
+    // plane for player to spawn in
     int i = 0;
     while (i < SIZE) {
         int j = 0;
         while (j < SIZE) {
             if(i == spawn_x && j == spawn_y){
-                printf("N ");
-
+                printf("\033[1;33m");//Set the text to the color red
+                printf("B "); //Display Butch in yellow
+                printf("\033[0m");
             }
             else{
                 printf("%d ", map[i][j]);
@@ -33,10 +35,7 @@ void print_map (int map[SIZE][SIZE]) {
         i++;
     }
 }
-void spawn_player(int map[SIZE][SIZE]){
 
-    
-}
 void bomb_explosion (int radius, int map[SIZE][SIZE], int startx, int starty) {
     // I am going to map out the area that an explosion from a bomb the
     // player can use that will create a circle bomb radius
@@ -54,7 +53,7 @@ void bomb_explosion (int radius, int map[SIZE][SIZE], int startx, int starty) {
         }
     }
 }
-void check_for_game_end (int map[SIZE][SIZE], int laser_y) {
+void check_for_game_end (int map[SIZE][SIZE], int laser_y, int spawn_x, int spawn_y) {
     int x;
     int y;
     int counter = 0;
@@ -65,16 +64,51 @@ void check_for_game_end (int map[SIZE][SIZE], int laser_y) {
             }
         }
     }
-    if (counter == 399) {
-        print_map (map);
+    if (counter == 144) {
+        print_map (map, spawn_x, spawn_y);
         printf ("Game Won!\n");
         exit (0);
     }
 }
 int main(void) {
-    
+    int direction;
+    int spawn_x = random_number(12,0);
+    int spawn_y = random_number(12,0);
     int map[SIZE][SIZE] = {EMPTY};
-    print_map(map);
+    print_map(map, spawn_x, spawn_y);
+    printf("You are Butch, a member of the [redacted] military tasked taking down human hybrid bioweapons that escaped from a top-secret testing facility in [redacted].\n");
+    printf("You are tasked with rescuing civilians and taking down these enemies with your long ranged sniper.\n ");
+    printf("Enter Direction using numpad\n");
+    printf("Press 8 to go up\n");
+    printf("Press 2 to go down\n");
+    printf("Press 4 to go left\n");
+    printf("Press 6 to go right\n");
+    int keeplooping = 1;
+    while(keeplooping == 1){
+        scanf("%d", &direction);
+        if(direction == 2) {
+            if(spawn_x == 12) {
+                printf("Butch: I can't leave and let those monsters wreak havoc on the people nearby!\n");
+                spawn_x--;
+            }
+            spawn_x++;
+            
+        }
+        else if(direction == 4) {
+            spawn_y--;
+        }
+        else if(direction == 6) {
+            spawn_y++;
+        }
+        else if(direction == 8) {
+            spawn_x--;
+        }
+        print_map(map, spawn_x, spawn_y);
+
+
+    }
+
+    
     
   
     
