db56963f6f247d8eebb75b27b18c9858981e0760
Zunnoon Tahir <78951403+Zunnoont@users.noreply.github.com>
2021-06-07 15:37:46 +1000

    Added random spawn points
    Player will spawn at random making each play-through unique

--- locked_n_loaded.c
+++ locked_n_loaded.c
@@ -1,45 +1,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
 
-#define SIZE 20
+#define SIZE 13
 #define EMPTY 0
-#define STONE 1
-
+#define ENEMY 1
+int random_number(int upper, int lower){
+    srand(time(0));
+    int random_value =  (rand() % (upper - lower + 1)) + lower;
+    return random_value;
+}
 void print_map (int map[SIZE][SIZE]) {
+    int spawn_x = random_number(12,0);
+    int spawn_y = random_number(12,0);
+   
     int i = 0;
     while (i < SIZE) {
         int j = 0;
         while (j < SIZE) {
-            printf("%d ", map[i][j]);
+            if(i == spawn_x && j == spawn_y){
+                printf("N ");
+
+            }
+            else{
+                printf("%d ", map[i][j]);
+            }
             j++;
         }
         printf("\n");
         i++;
     }
 }
-void tnt_explosion (int radius, int map[SIZE][SIZE], int startx, int starty) {
-    // I am going to map out the area that an explosion from tnt will create in a circle
+void spawn_player(int map[SIZE][SIZE]){
+
+    
+}
+void bomb_explosion (int radius, int map[SIZE][SIZE], int startx, int starty) {
+    // I am going to map out the area that an explosion from a bomb the
+    // player can use that will create a circle bomb radius
     int row;
     int column;
     for (row = (startx - radius); 
     row <= (startx + radius); row++) {
-        // I am using the mathematical formula (x-a)^2 +(y-k)^2 =  r^2     
-        // Since the position isnt always in the center, using this formula is essential 
-        //to consider the range of the tnt
-        // Using inequailities (and abit of desmos), 
-        // it becomes aparrent that if you create an inequality with this equation, 
-        // you can shade the inside
-        // So essentially, as long as the sum is less than r^2, 
-        // it should be inside the circle
         for (column = (starty - radius); column < (starty + radius); column++) {
             if ((row-startx) * (row-startx) + 
             (column-starty) * (column-starty) < (radius*radius)
             && row >= 0 && row < SIZE && column >= 0 && column < SIZE)
             {
                 map[row][column] = EMPTY;
-                
             }
         }
     }
@@ -55,14 +65,19 @@ void check_for_game_end (int map[SIZE][SIZE], int laser_y) {
             }
         }
     }
-    if (counter == 625) {
+    if (counter == 399) {
         print_map (map);
         printf ("Game Won!\n");
         exit (0);
     }
 }
-int main(void){
+int main(void) {
+    
     int map[SIZE][SIZE] = {EMPTY};
     print_map(map);
+    
+  
+    
+   
 
 }
